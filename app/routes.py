9a8ad7d88cccd3aa68d341dd855eb3963210d527
Deskola <denisogunde@gmail.com>
2019-07-09 12:14:32 +0300

    creating template folder

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hellow"
+    user = {'username': 'Dennis', 'email':'denis@gmail.com'}
+    post = [
+        {
+            'author':'Denis',
+            'body':'This is a nice day mate'
+        },
+        {
+            'author':'Tony',
+            'body':'This is a nice day in Kenya @Denis'
+        }
+    ]
+    return render_template('index.html',  user=user, post=post)
